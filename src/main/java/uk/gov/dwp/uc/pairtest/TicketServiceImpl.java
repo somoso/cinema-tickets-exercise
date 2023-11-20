c2bd405245c796eb1c21e6af0142aee278c186c4
Soheb <git@sohebm.com>
2023-11-20 15:36:06 +0000

    Initial commit

---
+++ src/main/java/uk/gov/dwp/uc/pairtest/TicketServiceImpl.java
@@ -0,0 +1,16 @@
+package uk.gov.dwp.uc.pairtest;
+
+import uk.gov.dwp.uc.pairtest.domain.TicketPurchaseRequest;
+import uk.gov.dwp.uc.pairtest.exception.InvalidPurchaseException;
+
+
+public class TicketServiceImpl implements TicketService {
+
+    /**
+     * Should only have private methods other than the one below.
+     */
+    @Override
+    public void purchaseTickets(TicketPurchaseRequest ticketPurchaseRequest) throws InvalidPurchaseException {
+
+    }
+}
